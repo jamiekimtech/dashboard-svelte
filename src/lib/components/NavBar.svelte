d6ee2f1897edc35fcec15747cfd24eead024b554
Jamie Kim <jamiekimtech313@gmail.com>
2023-07-11 22:34:15 -0400

    Sveltekit dashboard

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,18 @@
+<div class="navbar">
+	<nav>
+		<li><a href="/dashboard">Dashboard</a></li>
+		<li><a href="/templates">Templates</a></li>
+		<li><a href="/admin-panels">Admin Panels</a></li>
+		<li><a href="/apps">Apps</a></li>
+		<li><a href="/pages">Pages</a></li>
+		<li><a href="/general">General</a></li>
+		<li><a href="/user-interface">User Interface</a></li>
+		<li><a href="/forms">Forms</a></li>
+		<li><a href="/tables">Tables</a></li>
+		<li><a href="/add-ons">Add Ons</a></li>
+		<li><a href="/charts">Charts</a></li>
+		<li><a href="/support">Support</a></li>
+		<li><a href="/document">Document</a></li>
+	</nav>
+	<div class="burger" />
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Header from './Header.svelte';
+	import NavBar from '../lib/components/NavBar.svelte';
+</script>
+
+<Header />
+<slot />
+
+<style>
+	@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CQuicksand:300,400,500,700');
+
+	:global(*) {
+		font-family: 'Open Sans';
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		text-decoration: none;
+		--primary: #7c8697;
+		color: var(--primary);
+	}
+</style>
