223db253eb7caecb9698f4eca351db1e52822210
Jamie Kim <jamiekimtech313@gmail.com>
2023-07-19 16:31:12 -0400

    Add elements to main page(mobile)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,13 @@
 	import { revenue } from '$lib/data/chartObj.json';
 	import { onMount } from 'svelte';
 	import { Chart, registerables } from 'chart.js';
+	import Icon from '@iconify/svelte';
 
 	Chart.register(...registerables);
 
 	let lineChartElement;
-	let pieChartElement;
+	let pieChartElement1;
+	let pieChartElement2;
 
 	const chartData1 = {
 		labels: revenue.map(({ day }) => day),
@@ -24,7 +26,17 @@
 			{
 				data: [18, 82],
 				backgroundColor: ['#28d094', '#ff4961'],
-				borderWidth: 0
+				borderColor: ['#ffffff'],
+				borderWidth: 1
+			}
+		]
+	};
+	const chartData3 = {
+		datasets: [
+			{
+				data: [24, 76],
+				backgroundColor: ['#ff7b8c', '#ffffff'],
+				borderColor: ['transparent']
 			}
 		]
 	};
@@ -44,35 +56,34 @@
 						x: {
 							grid: {
 								color: 'hsl(43 100% 52% / 0%)'
-							},
-							ticks: { color: '#000' }
+							}
 						},
 						y: {
-							beginAtZero: true,
-							ticks: { color: '#000', font: { size: 13 }, stepSize: 5000, max: 20000 },
-
-							grid: {
-								color: 'hsl(0, 1%, 89%)'
-							}
+							beginAtZero: true
 						}
 					}
 				}
 			});
 
-			new Chart(pieChartElement, {
+			new Chart(pieChartElement1, {
 				type: 'doughnut',
 				data: chartData2,
 				options: {
-					borderRadius: '20',
+					borderRadius: '10',
 					responsive: true,
-					cutout: '95%',
-					spacing: 5,
-					scales: {
-						y: {
-							beginAtZero: true,
-							ticks: { color: '#fff', font: { size: 0 }, stepSize: 5000, max: 20000 }
-						}
-					}
+					cutout: '93%',
+					spacing: 3
+				}
+			});
+
+			new Chart(pieChartElement2, {
+				type: 'doughnut',
+				data: chartData3,
+				options: {
+					borderRadius: '10',
+					responsive: true,
+					cutout: '93%',
+					spacing: 3
 				}
 			});
 		}
@@ -81,32 +92,76 @@
 
 <main>
 	<div class="card">
-		<h5>Revenue</h5>
-		<br />
+		<h6>Revenue</h6>
 		<div class="card-data">
 			<div>
-				<h6>Current Week</h6>
-				<h2>$82,124</h2>
+				<h7>Current Week</h7>
+				<h2 class="red">$82,124</h2>
 			</div>
 			<div>
-				<h6>Previous Week</h6>
+				<h7>Previous Week</h7>
 				<h2>$52,502</h2>
 			</div>
 		</div>
 		<canvas bind:this={lineChartElement} />
 	</div>
-	<canvas bind:this={pieChartElement} />
+	<div class="card">
+		<h6>
+			Hit Rate <span class="red">-12%</span>
+		</h6>
+		<h1 class="red">82%</h1>
+		<canvas bind:this={pieChartElement1} />
+	</div>
+	<div class="card red-bg">
+		<h6>Deals -55%</h6>
+		<h1 class="red-bg">76%</h1>
+		<canvas bind:this={pieChartElement2} />
+	</div>
+	<div class="card">
+		<div class="flex">
+			<div>
+				<h7>Order Value</h7>
+				<h3>$ 88,568</h3>
+			</div>
+			<Icon icon="simple-line-icons:trophy" width="45px" color="#28d094" />
+		</div>
+	</div>
+	<div class="card">
+		<div class="flex">
+			<div>
+				<h7>Calls</h7>
+				<h3>3,568</h3>
+			</div>
+			<Icon icon="basil:phone-in-outline" width="50px" color="red" />
+		</div>
+	</div>
 </main>
 
 <style>
 	main {
 		color: var(--primary);
+		padding: 22px;
 	}
 	.card {
 		padding: 20px;
+		margin: 15px 0 30px 0;
+		box-shadow: 0px 14px 39px -17px rgba(0, 0, 0, 0.15);
+	}
+
+	.card h6 {
+		font-weight: 800;
+		letter-spacing: 0.5px;
+	}
+
+	.card h1 {
+		font-weight: 900;
+		font-size: 35px;
+		letter-spacing: 1px;
+		position: absolute;
+		top: 50%;
+		left: 45%;
 	}
 	canvas {
-		background-color: #fff;
 		padding: 10px;
 	}
 
@@ -114,6 +169,26 @@
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
-		width: 60%;
+		width: 95%;
+		margin-top: 10px;
+	}
+
+	.red {
+		color: #ff4961;
+	}
+
+	.green {
+		color: #28d094;
+	}
+
+	.red-bg {
+		color: white;
+		background-color: #ff4961;
+	}
+
+	.flex {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 	}
 </style>
